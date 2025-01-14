b5cb3cedf3211f4c1cbd83268882112a155b47d4
Khan Mubashshir Azeem <khanmubashshir08@gmail.com>
2025-01-14 19:45:51 +0530

    taking  more data of user

--- app/src/main/java/com/example/skills_plus/adapter/AllBlogAdapter.java
+++ app/src/main/java/com/example/skills_plus/adapter/AllBlogAdapter.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
